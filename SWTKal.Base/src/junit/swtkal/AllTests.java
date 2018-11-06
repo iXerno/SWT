d4e30459a57c6e69fd05e2604260c15732cfaa15
jo12594 <jo12594@swt-20.win.local>
2018-11-06 14:10:38 +0100

    test

--- SWTKal.Base/src/junit/swtkal/AllTests.java
+++ SWTKal.Base/src/junit/swtkal/AllTests.java
@@ -17,7 +17,7 @@ import junit.framework.TestSuite;
 
 
 /*****************************************************************************************************
- * This AllTests .......
+ * This AllTests ..........
  * 
  * @author calproj
  */
