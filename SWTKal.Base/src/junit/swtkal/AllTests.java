4de8af3d4b5b985b2d34e59bcdda87614b5e28c8
iXerno <33544999+iXerno@users.noreply.github.com>
2018-11-06 14:16:34 +0100

    Update AllTests.java

--- SWTKal.Base/src/junit/swtkal/AllTests.java
+++ SWTKal.Base/src/junit/swtkal/AllTests.java
@@ -17,7 +17,7 @@ import junit.framework.TestSuite;
 
 
 /*****************************************************************************************************
- * This AllTests ..........Test
+ * This AllTests ..........
  * 
  * @author calproj
  */
